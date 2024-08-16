3a5793519acfe863b6a4561020c97012e9161d01
Jérôme Bousquié <jerome.bousquie@iut-rodez.fr>
2024-08-16 20:54:28 +0200

    ajout y_sort

--- src/game.rs
+++ src/game.rs
@@ -1,30 +1,46 @@
 use macroquad::prelude::*;
+use std::rc::Rc;
 
+#[derive(Debug)]
 pub struct Sprite {
+    pub id: u32,
     pub x: f32,
     pub y: f32,
     pub width: f32,
     pub height: f32,
-    pub texture_id: usize,
+    pub texture: Rc<Texture2D>,
     pub color: Color,
 }
 
-pub fn ysort_sprites(mut sprites: Vec<Sprite>) {
+impl Sprite {
+    pub fn move_to(&mut self, x: f32, y: f32) -> &mut Sprite {
+        self.x = x;
+        self.y = y;
+        self
+    }
+}
+
+pub fn ysort_sprites(mut sprites: Vec<Sprite>) -> Vec<Sprite> {
     sprites.sort_by(|a, b| (cmp_sprites(&a, &b)));
+    sprites
 }
 
 fn cmp_sprites(a: &Sprite, b: &Sprite) -> std::cmp::Ordering {
-    (a.y as i32).cmp(&(b.y as i32))
+    ((a.y + a.height) as i32).cmp(&((b.y + b.height) as i32))
 }
-pub fn cull_sprites() {}
+//pub fn cull_sprites() {}
 
-pub fn draw_sprites(sprites: Vec<Sprite>, textures: Vec<Texture2D>) {
+pub fn draw_sprites(sprites: &Vec<Sprite>) {
     for sprite in sprites {
-        draw_texture(
-            &textures[sprite.texture_id],
+        draw_texture_ex(
+            &sprite.texture,
             sprite.x,
             sprite.y,
             sprite.color,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(sprite.width, sprite.height)),
+                ..Default::default()
+            },
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,42 @@
+use game::{draw_sprites, ysort_sprites};
 use macroquad::prelude::*;
+use std::rc::Rc;
 mod game;
 mod player;
 mod world;
 use player::player::move_player;
 use world::world::create_world;
 
-const BLOCK_SIZE: f32 = 30.0;
-
 #[macroquad::main("Game1")]
 async fn main() {
-    let blocks = create_world();
-    let textures = vec![load_texture("images/rustacean_happy.png").await.unwrap()];
+    let mut sprites: Vec<game::Sprite> = Vec::new();
+    let texture_happy = Rc::new(load_texture("images/rustacean_happy.png").await.unwrap());
+    sprites = create_world(sprites, &texture_happy);
 
     let player_sprite = game::Sprite {
+        id: 1000,
         x: 150.0,
         y: 150.0,
-        width: 30.0,
-        height: 30.0,
-        texture_id: 0,
+        width: 50.0,
+        height: 50.0,
+        texture: texture_happy.clone(),
         color: WHITE,
     };
-
+    sprites.push(player_sprite);
     let mut x = 150.0;
     let mut y = 150.0;
+
     loop {
         clear_background(GRAY);
         (x, y) = move_player(x, y);
-        draw_texture(&texture, x, y, WHITE);
-        for block in &blocks {
-            draw_rectangle(
-                block.x as f32,
-                block.y as f32,
-                BLOCK_SIZE,
-                BLOCK_SIZE,
-                GREEN,
-            );
+        for sprite in &mut sprites {
+            if sprite.id == 1000 {
+                sprite.move_to(x, y);
+            }
         }
+        sprites = ysort_sprites(sprites);
+        draw_sprites(&sprites);
+
         next_frame().await
     }
 }
